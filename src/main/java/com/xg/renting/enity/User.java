49376fc47ecdd74ee070a688158bb8bd0fd8e0ba
Yan <1175842607@qq.com>
2021-01-21 18:54:42 +0800

    test

--- src/main/java/com/xg/renting/enity/User.java
+++ src/main/java/com/xg/renting/enity/User.java
@@ -8,6 +8,7 @@ public class User {
     //用户属性：用户ID、用户名、用户密码、用户电话、用户类型
     private int user_id;
     private String user_name;
+    private int open_id;
     private String user_password;
     private int user_telephone;
     private int user_role;
