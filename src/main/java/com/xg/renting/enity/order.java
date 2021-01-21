80d97baf4b736538fe4dcc2048bd7f1fd68a838c
Yan <1175842607@qq.com>
2021-01-21 16:04:31 +0800

    test

--- src/main/java/com/xg/renting/enity/order.java
+++ src/main/java/com/xg/renting/enity/order.java
@@ -6,7 +6,7 @@ import lombok.Data;
 public class order {
     //订单id、订单名称、租户id、商户id、订单价格、入住时间、租期、订单状态（0 待支付、1已支付）
     private int order_id;
-    private String order_name;
+//    private String order_name;
     private int tenant_id;
     private int merchant_id;
     private float order_price;
