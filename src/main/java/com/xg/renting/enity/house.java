980e2c20126deb85a3536608121c1c29a85fc0e8
Yan <1175842607@qq.com>
2021-01-21 15:49:01 +0800

    test

---
+++ src/main/java/com/xg/renting/enity/house.java
@@ -0,0 +1,14 @@
+package com.xg.renting.enity;
+
+import lombok.Data;
+
+@Data
+public class house {
+
+    private int house_id;
+    private String house_title;          //标题，房型等
+    private String house_information;   //具体信息：地址，房子介绍
+    private int merchant_id;
+    private float house_price;
+    private int house_lease;
+}
---
+++ src/main/java/com/xg/renting/enity/order.java
@@ -0,0 +1,16 @@
+package com.xg.renting.enity;
+
+import lombok.Data;
+
+@Data
+public class order {
+    //订单id、订单名称、租户id、商户id、订单价格、入住时间、租期、订单状态（0 待支付、1已支付）
+    private int order_id;
+    private String order_name;
+    private int tenant_id;
+    private int merchant_id;
+    private float order_price;
+    private String in_time;
+    private int lease_term;
+    private int order_status;
+}
