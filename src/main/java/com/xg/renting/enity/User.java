64b18949cd9940cf0f6324672d38b353f0877155
Yan <1175842607@qq.com>
2021-01-21 15:00:12 +0800

    test

--- src/main/java/com/xg/renting/enity/User.java
+++ src/main/java/com/xg/renting/enity/User.java
@@ -11,19 +11,4 @@ public class User {
     private String user_password;
     private int user_role;
 
-    public String getUser_name() {
-        return user_name;
-    }
-
-    public void setUser_name(String user_name) {
-        this.user_name = user_name;
-    }
-
-    public String getUser_password() {
-        return user_password;
-    }
-
-    public void setUser_password(String user_password) {
-        this.user_password = user_password;
-    }
 }
