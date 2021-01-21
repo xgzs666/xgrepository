7cc8818e36b19ef14b2243e26ca7bce5b721d935
Yan <1175842607@qq.com>
2021-01-21 20:03:35 +0800

    test

---
+++ src/main/java/com/xg/renting/enity/experience.java
@@ -0,0 +1,14 @@
+package com.xg.renting.enity;
+
+import lombok.Data;
+
+@Data
+public class experience {
+    private int experience_id;
+    private String experience_title;
+    private String experience_content;
+    private String experience_answer;
+    private int user_id;
+
+
+}
