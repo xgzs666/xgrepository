fba264029da1be58959f576f9769ca26a24a8c86
Yan <1175842607@qq.com>
2021-01-21 14:29:22 +0800

    test

--- src/main/java/com/xg/renting/enity/HelloWorld.java
+++ src/main/java/com/xg/renting/enity/HelloWorld.java
@@ -2,6 +2,6 @@ package com.xg.renting.enity;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("HelloWor7d");
     }
 }
