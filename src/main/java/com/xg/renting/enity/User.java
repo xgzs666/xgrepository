1ed59a7d88db06cc51ad23e7fec898f30ee73c89
Yan <1175842607@qq.com>
2021-01-21 15:48:18 +0800

    test

--- src/main/java/com/xg/renting/enity/User.java
+++ src/main/java/com/xg/renting/enity/User.java
@@ -5,10 +5,11 @@ import lombok.Data;
 @Data
 public class User {
 
-    //用户属性：用户ID、用户名、用户密码、用户类型
+    //用户属性：用户ID、用户名、用户密码、用户电话、用户类型
     private int user_id;
     private String user_name;
     private String user_password;
+    private int user_telephone;
     private int user_role;
 
 }
